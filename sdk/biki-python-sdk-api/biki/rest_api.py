092f2ee10a0ca6c682a48c7b441c1d523a0e9f9e
hudiefei <hudiefei@biki.com>
2019-07-26 23:38:47 +0800

    modified websocket demo

--- sdk/biki-python-sdk-api/biki/rest_api.py
+++ sdk/biki-python-sdk-api/biki/rest_api.py
@@ -3,7 +3,7 @@ from .consts import *
 import json
 
 
-class SpotAPI(Client):
+class RestAPI(Client):
 
     def __init__(self, api_key, api_seceret_key):
         Client.__init__(self, api_key, api_seceret_key)
@@ -11,7 +11,7 @@ class SpotAPI(Client):
     # query account balance
     def get_account(self):
         params = {}
-        return self._request_with_params(GET, SPOT_ACCOUNT, params)
+        return self._request_with_params(GET, REST_ACCOUNT, params)
 
     # query all order
     def get_all_order(self, symbol, startdate=None, enddate=None, pagesize=None, page=None, sort=None):
@@ -27,7 +27,7 @@ class SpotAPI(Client):
             params['page'] = page
         if sort:
             params['sort'] = sort
-        return self._request_with_params(GET, SPOT_ALL_ORDER, params)
+        return self._request_with_params(GET, REST_ALL_ORDER, params)
 
     #query all trade
     def get_all_trade(self, symbol, startdate=None, enddate=None, pagesize=None, page=None, sort=None):
@@ -43,20 +43,20 @@ class SpotAPI(Client):
             params['page'] = page
         if sort:
             params['sort'] = sort
-        return self._request_with_params(GET, SPOT_ALL_TRADE, params)
+        return self._request_with_params(GET, REST_ALL_TRADE, params)
 
     #cancel order by order_id
     def cancel_order(self, order_id, symbol):
         params = {}
         params['order_id'] = order_id
         params['symbol'] = symbol
-        return self._request_with_params(POST, SPOT_CANCEL_ORDER, params)
+        return self._request_with_params(POST, REST_CANCEL_ORDER, params)
 
     #cancel all order by symbol
     def cancel_order_all(self, symbol):
         params = {}
         params['symbol'] = symbol
-        return self._request_with_params(POST, SPOT_CANCEL_ORDER_ALL, params)
+        return self._request_with_params(POST, REST_CANCEL_ORDER_ALL, params)
 
     #create order
     def create_order(self, symbol, type, side, volume, price = None, fee_is_user_exchange_coin='0'):
@@ -69,43 +69,43 @@ class SpotAPI(Client):
         params['fee_is_user_exchange_coin'] = fee_is_user_exchange_coin
         if price:
             params['price'] = price
-        return self._request_with_params(POST, SPOT_CREATE_ORDER, params)
+        return self._request_with_params(POST, REST_CREATE_ORDER, params)
 
     #qurey all symbol ticker
     def get_all_ticker(self):
         params = {}
-        return self._request_no_sign_params(GET, SPOT_ALL_TICKER, params)
+        return self._request_no_sign_params(GET, REST_ALL_TICKER, params)
 
     #qurey k-line records
     def get_records(self, symbol, period):
         params = {}
         params['symbol'] = symbol
         params['period'] = period
-        return self._request_no_sign_params(GET, SPOT_RECORDS, params)
+        return self._request_no_sign_params(GET, REST_RECORDS, params)
 
     #qurey ticker by symbol
     def get_ticker(self, symbol):
         params = {}
         params['symbol'] = symbol
-        return self._request_no_sign_params(GET, SPOT_TICKER, params)
+        return self._request_no_sign_params(GET, REST_TICKER, params)
 
     #qurey trades by symbol
     def get_trades(self, symbol):
         params = {}
         params['symbol'] = symbol
-        return self._request_no_sign_params(GET, SPOT_TRADES, params)
+        return self._request_no_sign_params(GET, REST_TRADES, params)
 
     #qurey the latest transaction price of each symbol of currencies
     def get_market(self):
         params = {}
-        return self._request_no_sign_params(GET, SPOT_MARKET, params)
+        return self._request_no_sign_params(GET, REST_MARKET, params)
 
     #qurey the depth of buying and selling
     def get_market_dept(self, symbol, type):
         params = {}
         params['symbol'] = symbol
         params['type'] = type
-        return self._request_with_params(GET, SPOT_DEPTH, params)
+        return self._request_with_params(GET, REST_DEPTH, params)
 
     #mass create and cancel order
     # params example:{'btcusdt',
@@ -123,7 +123,7 @@ class SpotAPI(Client):
                 request_params.append(param)
             params['mass_place'] = json.dumps(request_params)
         params['symbol'] = symbol
-        return self._request_with_params(POST, SPOT_MASS_REPLACE, params)
+        return self._request_with_params(POST, REST_MASS_REPLACE, params)
 
     #qurey the current order(including uncompleted and ongoing order)
     def get_new_order(self, symbol, pagesize=None, page=None):
@@ -133,16 +133,16 @@ class SpotAPI(Client):
             params['pageSize'] = pagesize
         if page:
             params['page'] = page
-        return self._request_with_params(GET, SPOT_NEW_ORDER, params)
+        return self._request_with_params(GET, REST_NEW_ORDER, params)
 
     #qurey order details by order_id
     def get_order_info(self, id, symbol):
         params = {}
         params['order_id'] = id
         params['symbol'] = symbol
-        return self._request_with_params(GET, SPOT_ORDER_INFO, params)
+        return self._request_with_params(GET, REST_ORDER_INFO, params)
 
     #qurey all symbols and precision
     def get_symbols(self):
         params = {}
-        return self._request_no_sign_params(GET, SPOT_SYMBOLS, params)
+        return self._request_no_sign_params(GET, REST_SYMBOLS, params)
